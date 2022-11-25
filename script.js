87cf803f00b653b113cc37b21fd6e286fa503278
Juma Koga <jumakoga5962@gmail.com>
2022-11-25 11:07:24 +0100

    couleurs saisons titre

--- script.js
+++ script.js
@@ -37,15 +37,35 @@ function startTime() {
   var njour = today.getDate();
   var mois = today.getMonth();
   var jour = today.getDay();
+  var start = new Date(today.getFullYear(), 0, 0);
+  var diff = today - start;
+  var oneDay = 1000 * 60 * 60 * 24;
+  var day = Math.floor(diff / oneDay);
+  var couleur = convertColor(day);
   jour = convertJ(jour);
   mois = convertM(mois+1);
   h = checkTime(h);
   m = checkTime(m);
   document.getElementById('time').innerHTML =
   jour + " " + njour + " " + mois + "  -  " + h + ":" + m;
-  document.getElementById('time').dataset.datatext = 
-  jour + " " + njour + " " + mois + "  -  " + h + ":" + m;
+  document.getElementById("gradient").style.background = couleur;
   var t = setTimeout(startTime, 500);
+}
+function convertColor(jour) {
+  var couleur;
+  if (jour >= 79 && jour < 171) {
+    couleur = "linear-gradient(45deg, rgb(51, 144, 2), rgb(133, 255, 108))";
+  }
+  else if (jour >= 171 && jour < 265) {
+    couleur = "linear-gradient(45deg, yellow, rgb(3, 208, 254))";
+  }
+  else if (jour >= 265 && jour < 355) {
+    couleur = "linear-gradient(45deg, red, yellow)";
+  }
+  else {
+    couleur = "linear-gradient(45deg, rgb(57, 229, 255), rgb(234, 255, 254))";
+  }
+  return couleur;
 }
 function checkTime(i) {
   if (i < 10) {i = "0" + i};
