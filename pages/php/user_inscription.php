7cfc8a4b3bf5fba778ec49857755fdaa927e2016
FailornB <f.beyen@orange.fr>
2022-11-28 15:12:30 +0100

    Oui

--- pages/php/user_inscription.php
+++ pages/php/user_inscription.php
@@ -1,19 +1,104 @@
-<?php
-  $bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8','root', '');
-  if(isset($_POST['valider'])){
-    $name => $_FILES["image"]["name"];
-    $type => $_FILES["image"]["type"];
-    $data => file_get_contents($_FILES["image"]["tmp_name"]);
-    $insert=$bdd->prepare('INSERT INTO myblob VALUES ('',?,?,?)');
-    $insert->bindParam(1, $name);
-    $insert->bindParam(2, $type);
-    $insert->bindParam(3, $data);
-    $insert->execute();
-  }
-?>
+<?php 
+    $bdd = mysqli_connect("localhost", "utilisateur", "projetweb2022", "projet");// On inclut la connexion à la bdd
+    // Si les variables existent et qu'elles ne sont pas vides
+    if(isset($_POST['upload'])){
+      $image = $_FILES['image']['name'];
+      $path = '../../img_compte/'.$image;
+      $nom = htmlspecialchars($_POST['Nom']);
+      $prenom = htmlspecialchars($_POST['Prenom']);
+      $type_compte = htmlspecialchars($_POST['TypeCompte']);
+      $num_etud = htmlspecialchars($_POST['NumEtu']);
+      $email = htmlspecialchars($_POST['Mail']);
+      $password = htmlspecialchars($_POST['Mdp']);
+      //$check = $bdd->prepare('SELECT Nom,Prenom, Mail, NumEtu, Mdp FROM user WHERE Mail = ?');
+      //$check->execute([$email]);
+      //$data = $check->fetch();
+      //$row = $check->rowCount();
 
+      $email = strtolower($email);
+      //$req2="SELECT * FROM compte WHERE Adresse = '".$EMAIL."'";// requete de la verif de l'existence de l'email dans la BDD
+       //         $verif2=mysqli_query($connect,$req2);//execution de la requette
+                 
+      //          if($verif2){//si adresse présente dans la BDD
+          //          echo "L'adresse possede deja un compte sur ce site...<br/>";                   
+         //       }
+      $select = mysqli_query($bdd, "SELECT * FROM user WHERE Mail = '".$email."'");
+      if(mysqli_num_rows($select)){
+        header('Location: user_inscription.php?reg_err=already'); 
+        die();
+      }
+      if(filter_var($email, FILTER_VALIDATE_EMAIL))  
+      { // Si l'email est de la bonne forme
+            // On hash le mot de passe
+        $password = hash('sha256', $password);
+        $sql = $bdd->query("INSERT INTO user(NumEtu, Nom, Prenom, Mail, Mdp, image, TypeCompte) VALUES ('$num_etud','$nom','$prenom','$email','$password','$path','$type_compte')");
+        move_uploaded_file($_FILES['image']['tmp_name'], $path);
+        header('Location:user_inscription.php?reg_err=success');
+        die();
+
+      }else{ header('Location: user_inscription.php?reg_err=email'); die();}
+    }
+        //$email = strtolower($email);
+        //$nom = htmlspecialchars($_POST['Nom']);
+        //$prenom = htmlspecialchars($_POST['Prenom']);
+        //$image = $_FILES['image']['name'];
+        //$picture = htmlspecialchars($_POST['image']);
+        //$target = "uploads/".basename($image);
+        //$sql = "INSERT INTO image_upload (image) VALUES ('$image')";
+        //mysqli_query($db, $sql);
+  	    //move_uploaded_file($_FILES['image']['tmp_name'], $target);
+
+        //$type_compte = htmlspecialchars($_POST['TypeCompte']);
+        //$num_etud = htmlspecialchars($_POST['NumEtu']);
+        //$email = htmlspecialchars($_POST['Mail']);
+        //$password = htmlspecialchars($_POST['Mdp']);
+
+        // On vérifie si l'utilisateur existe
+        //$check = $bdd->prepare('SELECT Nom,Prenom, Mail, NumEtu, Mdp FROM user WHERE Mail = ?');
+        //$check->execute(array($email));
+        //$data = $check->fetch();
+        //$row = $check->rowCount();
+
+        //$email = strtolower($email);
+
+        // Si la requete renvoie un 0 alors l'utilisateur n'existe pas 
+        //if($row == 0)
+        //{ 
+         // if(filter_var($email, FILTER_VALIDATE_EMAIL))
+         // { // Si l'email est de la bonne forme
+                            // On hash le mot de passe
+           // $password = hash('sha256', $password);
+                            
+                            // On stock l'adresse IP
+            //$ip = $_SERVER['REMOTE_ADDR']; 
+                             /*
+      
+                              Verifiez bien que le champ token est présent dans votre table user
+
+                              ATTENTION
+                            */
+                            // On insère dans la base de données
+              //$insert = $bdd->prepare('INSERT INTO user(NumEtu, Nom, Prenom, Mail, Mdp, image, TypeCompte, Id, Token) VALUES(:NumEtu, :Nom, :Prenom, :Mail, :Mdp, :image, :TypeCompte, :Id, :Token)');
+              //$insert->execute(array(
+               // 'Nom' => $nom,
+              //  'Prenom' => $prenom,
+               // 'image' => $picture,
+               // 'TypeCompte' => $type_compte,
+               // 'NumEtu' => $num_etud,
+               // 'Mail' => $email,
+               // 'Mdp' => $password,
+               // 'Id' => $ip,
+               // 'Token' => bin2hex(openssl_random_pseudo_bytes(64))
+              //));
+                            // On redirige avec le message de succès
+              //header('Location:user_inscription.php?reg_err=success');
+              //die();
+
+         // }else{ header('Location: user_inscription.php?reg_err=email'); die();}
+       // }else{ header('Location: user_inscription.php?reg_err=already'); die();}
+    //  }
+?>
 <html>
-<!--peut etre un fichier html, pas besoin php-->
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
@@ -70,7 +155,7 @@
         }
     }
     ?>
-  <form name="formul" action="donnees_inscription.php" method="post" enctype="multipart/form-data">
+  <form method="POST" action="user_inscription.php" enctype="multipart/form-data">
     
       <h1>Inscription sur Ev'Note</h1>
       <hr>
@@ -82,8 +167,7 @@
       <input type="text" placeholder="Entrer votre prénom" name="Prenom" id="Prenom" required>
     
       <label for="picture"><b>Photo de profil</b></label>
-      <!--file ou image ??-->
-      <input type="file" placeholder="Mettre une photo de profil" name="image" accept="image/png, image/jpeg, image/jpg">
+      <input type="file" placeholder="Mettre une photo de profil" name="image" id="image" accept="image/png, image/jpeg, image/jpg" required>
     
       <div class="type_compte"></div>
       <label for="type_compte"><b>Type de compte</b></label>
@@ -105,7 +189,7 @@
       <!--demander numero etudiant seulement si compte de type etudiant -->
     
       <hr>
-      <button type="submit" name="valider" class="inscription">S'inscrire</button> 
+      <button type="submit" name="upload" class="inscription">S'inscrire</button>
   </form>
 </div>
 
