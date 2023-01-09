d42755987c2a473b72649507788b998386d8d4ff
Thibault Vercoutre <36052738+ThibaultVercoutre@users.noreply.github.com>
2023-01-09 19:14:15 +0100

    Revert "Julien"

--- pages/connexion/user_connexion.php
+++ pages/connexion/user_connexion.php
@@ -10,13 +10,7 @@
 <body>
   <header>
     <div id="titre">
-      <div id="effecten">
-        <h1 data-text="Ev'Note" id="evnote">Ev'Note</h1>
-        <div id = "gradient" class="gradient"></div>
-        <div class="spotlight"></div>
-      </div>
-      <span id="time-animation" class="material-symbols-outlined"></span>
-      <div id="time">Page de Connexion</div>
+      <h1>Ev'Note</h1>
     </div>
   </header>
   
@@ -78,4 +72,4 @@
 
 </body>
 </html>
-<script src="../../header.js"></script>
+
