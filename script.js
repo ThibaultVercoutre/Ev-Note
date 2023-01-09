c74cfabff8df60c2614134403b3cdf83ce278efc
FailornB <f.beyen@orange.fr>
2023-01-09 14:56:48 +0100

    like / dislike / report actu

--- script.js
+++ script.js
@@ -1605,6 +1605,7 @@ function addActionBdd(avis, nb, type, bool){
       console.log(type + " = " + data);
     });
 }
+
 function action_avis(e, save) {
 
   // like dislike report
@@ -1619,7 +1620,6 @@ function action_avis(e, save) {
   let Davis = document.getElementsByClassName("avi");
 
   for(var i = 0; i < Bup.length; i++) {
-    
     var avis = '';
     for(var j = 5; j < Davis[i].getAttribute("id").length; j++) {
       avis += Davis[i].getAttribute("id")[j];
@@ -1775,9 +1775,9 @@ function createFilActu(text){
   .then(data => {
     SartActu.innerHTML = '';
     Sdot.innerHTML = '';
-    for (var i = 0; i < Number(data[0]); i++) {
+    for (var i = 0; i < Number(data.length); i++) {
       Sdot.innerHTML += '<span class="dot" onclick="currentSlide(' + Number(i + 1) + ')"></span>'
-      SartActu.innerHTML += '<div class="mySlides fade"><div id="Article">'
+      SartActu.innerHTML += '<div class="mySlides fade" id="actu_' + data[i] + '"><div id="Article">'
                         + '<div class ="article-header">'
                           + '<img src="" class="avator">'
                           + '<div class="article-header-info">'
@@ -1793,16 +1793,20 @@ function createFilActu(text){
                           
                         + '<div class="article-info-counts">'
                           + '<div class="comments">'
-                            + '<svg class="feather feather-message-circle sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>'
+                            + '<span class="material-symbols-outlined comment">comment</span>'
                             + '<div class="comment-count">33</div>'
                           + '</div>'
                           + '<div class="likes">'
-                            + '<svg class="feather feather-heart sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>'
-                            + '<div class="likes-count">Pouce Bleu</div>'
+                            + '<span class="material-symbols-outlined up">thumb_up</span>'
+                            + '<div class="likes-count"></div>'
                           + '</div>'
-                          + '<div class="retweets">'
-                            + '<svg class="feather feather-repeat sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><polyline points="17 1 21 5 17 9"></polyline><path d="M3 11V9a4 4 0 0 1 4-4h14"></path><polyline points="7 23 3 19 7 15"></polyline><path d="M21 13v2a4 4 0 0 1-4 4H3"></path></svg>'
-                            + '<div class="report-count">Pouce Rouge</div>'
+                          + '<div class="dislikes">'
+                            + '<span class="material-symbols-outlined down">thumb_down</span>'
+                            + '<div class="dislikes-count"></div>'
+                          + '</div>'
+                          + '<div class="report">'
+                            + '<span class="material-symbols-outlined report">priority_high</span>'
+                            + '<div class="report-count"></div>'
                           + '</div>'
                         + '</div>'
                       + '</div></div>';
@@ -2059,7 +2063,8 @@ function createFilActu(text){
       }
     });
   }
-
+  
+  
   /* Report post */
   if(text != ''){
     var params = new URLSearchParams();
@@ -2094,6 +2099,42 @@ function createFilActu(text){
       }
     });
   }
+  /*Pouce Rouge post*/
+  if(text != ''){
+    var params = new URLSearchParams();
+    params.append('clave1', '18');
+    params.append('clave2', text);
+
+    fetch('/pages/fonctions_bdd/fonction_php_FA.php', {
+      method: 'POST',
+      body: params
+    })
+    .then(response => response.json())
+    .then(data => {
+      Srouge = document.querySelectorAll(".dislikes-count");
+      console.log(data);
+      for(var i = 0; i < data.length; i++){
+        Srouge[i].textContent = data[i];
+      }
+    });
+  }else{
+    var params = new URLSearchParams();
+    params.append('clave1', '19');
+    params.append('clave2', text);
+
+    fetch('/pages/fonctions_bdd/fonction_php_FA.php', {
+      method: 'POST',
+      body: params
+    })
+    .then(response => response.json())
+    .then(data => {
+      Srouge = document.querySelectorAll(".dislikes-count");
+      console.log(Srouge);
+      for(var i = 0; i < data.length; i++){
+        Srouge[i].textContent = data[i];
+      }
+    });
+  }
 }
 
 //createFilActu('');
@@ -2112,6 +2153,151 @@ function addFiltres(){
 }
 
 addFiltres();
+
+
+function modifActu(){
+    
+  
+}
+
+
+function addActionBdd_actu(actu, nb, type, bool){
+    
+    var user = document.getElementById('user').getAttribute("data");
+    fichier = 'pages/fonctions_bdd/maj_like_bdd_actu.php';
+
+    console.log(type);
+    /* Maj like */
+    var params = new URLSearchParams();
+    params.append('nombre', nb);
+    params.append('actu', actu);
+    params.append('type', type);
+    params.append('user', user);
+    params.append('bool', bool);
+
+    console.log(nb, actu, type, user, bool);
+
+    fetch(fichier, {
+      method: 'POST',
+      body: params
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(type + " = " + data);
+    });
+  
+}
+
+function action_actu(e, save) {
+
+
+// like dislike report
+let Bcomment = document.querySelectorAll(".comment");
+let Bup = document.querySelectorAll(".up");
+let Bdown = document.querySelectorAll(".down");
+let Breport = document.querySelectorAll(".report");
+
+let Ccomment = document.querySelectorAll(".comment-count");
+let Cup = document.querySelectorAll(".likes-count");
+let Cdown = document.querySelectorAll(".dislikes-count");
+let Creport = document.querySelectorAll(".report-count");
+
+let Dactu = document.getElementsByClassName("mySlides");
+
+for(var i = 0; i < Bup.length; i++) {
+  
+  var actu = '';
+  for(var j = 5; j < Dactu[i].getAttribute("id").length; j++) {
+    actu += Dactu[i].getAttribute("id")[j];
+  }
+
+  if(e == Bup[i]) {
+    if(Bup[i].style.color == "green"){
+      Bup[i].style.color = "black";
+      Cup[i].textContent = Number(Cup[i].textContent) - 1;
+      if(save == '0'){
+        addActionBdd_actu(actu, Cup[i].textContent, 'CptPouceBleu', 0);
+      }
+    }else{
+      Bup[i].style.color = "green";
+      Cup[i].textContent = Number(Cup[i].textContent) + 1;
+      if(save == '0'){
+        addActionBdd_actu(actu, Cup[i].textContent, 'CptPouceBleu', 1);
+      }
+    }
+
+    if(Bdown[i].style.color == "red"){
+      Bdown[i].style.color = "black";
+      Cdown[i].textContent = Number(Cdown[i].textContent) - 1;
+      if(save == '0'){
+        addActionBdd_actu(actu, Cdown[i].textContent, 'CptPouceRouge', 0);
+      }
+    }
+    
+    
+
+  }
+  if(e == Bdown[i]) {
+    if(Bdown[i].style.color == "red"){
+      Bdown[i].style.color = "black";
+      Cdown[i].textContent = Number(Cdown[i].textContent) - 1;
+      if(save == '0'){
+        addActionBdd_actu(actu, Cdown[i].textContent, 'CptPouceRouge', 0);
+      }
+    }else{
+      Bdown[i].style.color = "red";
+      Cdown[i].textContent = Number(Cdown[i].textContent) + 1;
+      if(save == '0'){
+        addActionBdd_actu(actu, Cdown[i].textContent, 'CptPouceRouge', 1);
+      }
+    }
+
+    if(Bup[i].style.color == "green"){
+      Bup[i].style.color = "black";
+      Cup[i].textContent = Number(Cup[i].textContent) - 1;
+      if(save == '0'){
+        addActionBdd_actu(actu, Cup[i].textContent, 'CptPouceBleu', 0);
+      }
+    }
+
+  }
+  if(e == Breport[i]) {
+    if(Breport[i].style.color == "orange"){
+      Breport[i].style.color = "black";
+      Creport[i].textContent = Number(Creport[i].textContent) - 1;
+      if(save == '0'){
+        addActionBdd_actu(actu, Creport[i].textContent, 'CptReport', 0);
+      }
+    }else{
+      Breport[i].style.color = "orange";
+      Creport[i].textContent = Number(Creport[i].textContent) + 1;
+      if(save == '0'){
+        addActionBdd_actu(actu, Creport[i].textContent, 'CptReport', 1);
+      }
+    }
+
+  }
+}
+}
+
+Savi.onclick = function(e){
+var user = document.getElementById('user').getAttribute("data");
+if(user != "0"){
+  action_avis(e.target, '0');
+}else{
+  alert("Vous devez être connecté pour pouvoir faire ça");
+}
+}
+
+Sactu.onclick = function(e){
+  var user = document.getElementById('user').getAttribute("data");
+  if(user != "0"){
+    action_actu(e.target, '0');
+  }else{
+    alert("Vous devez être connecté pour pouvoir faire ça");
+  }
+  }
+
 
 document.getElementById("BoutonEnvoieFiltres").addEventListener("click", addFiltres, false);
 
