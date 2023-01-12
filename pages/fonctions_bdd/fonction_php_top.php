c9f87e430287789abdea8da87a502edb7cf2c341
ThibaultVercoutre <thibault.vercoutre@yahoo.com>
2023-01-12 14:08:38 +0100

    fin top 10

--- pages/fonctions_bdd/fonction_php_top.php
+++ pages/fonctions_bdd/fonction_php_top.php
@@ -1,6 +1,7 @@
 <?php
 require_once '../php/login.php';
 $type = $_POST['type'];
+$id_lieu = $_POST['id_lieu'];
 
 /* liste Adresse Avis */
 
@@ -41,8 +42,44 @@ $stmt->execute();
 $resultats = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
 $lieux_photo = $resultats;
 
+/* liste Id Avis */
+
+$sql = "SELECT lieu.id_lieu
+        FROM lieu
+        JOIN avis ON avis.id_lieu = lieu.id_lieu
+        GROUP BY lieu.Adresse
+        ORDER BY AVG(CptEtoile) DESC";
+
+$stmt = $bdd->prepare($sql);
+$stmt->execute();
+$resultats = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+$id_lieux = $resultats;
+
+/* Lieu Clique */
+
+$sql = "SELECT Adresse
+        FROM lieu
+        WHERE id_lieu = ?";
+
+$stmt = $bdd->prepare($sql);
+$stmt->execute([$id_lieu]);
+$resultats = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+$Nom_lieu = $resultats;
+
+/* Ville clique */
+
+$sql = "SELECT Ville
+        FROM lieu
+        WHERE id_lieu = ?";
+
+$stmt = $bdd->prepare($sql);
+$stmt->execute([$id_lieu]);
+$resultats = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+$Ville_lieu = $resultats;
+
 switch($type){
-    case '0': echo(json_encode([$lieux, $lieux_note, $lieux_photo])); break;
+    case '0': echo(json_encode([$lieux, $lieux_note, $lieux_photo, $id_lieux])); break;
+    case '1': echo(json_encode([$Nom_lieu[0], $Ville_lieu[0]])); break;
 }
 
 ?>
